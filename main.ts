e63ae9e54b0bbf65d3fbaa9ca71ddcd2b4302854
johandquintero@utp.edu.co <johandquintero@utp.edu.co>
2023-08-29 11:53:05 -0500

    adding main

---
+++ main.ts
@@ -0,0 +1,27 @@
+import { Person, SinglePerson } from "./single_responsibility_principle_examples/singleResponsibilityPrinciple";
+import {
+    EmailSender,
+    SMSSender,
+    PushNotificationSender,
+    NotificationService,
+  } from './dependency_inversion_principle_examples/dependencyInversionPrinciple';
+
+//Using Single Responsibility Principle
+const person = new Person('Pablo', 12, 'hired');
+console.log(person.age);
+console.log(person.introduce());
+
+//Using Dependency Inversion Principle
+const emailSender = new EmailSender();
+const smsSender = new SMSSender();
+const pushSender = new PushNotificationSender();
+
+// Create NotificationService instances using different senders
+const emailService = new NotificationService(emailSender);
+const smsService = new NotificationService(smsSender);
+const pushService = new NotificationService(pushSender);
+
+// Use NotificationService to send notifications
+emailService.sendNotification('Hello via email');
+smsService.sendNotification('Hello via SMS');
+pushService.sendNotification('Hello via push notification');
--- single_responsibility_principle_examples/validScenarioImplementation.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var singleResponsibilityPrinciple_1 = require("./singleResponsibilityPrinciple");
-var person = new singleResponsibilityPrinciple_1.Person('Pablo', 12, 'hired');
-console.log(person.age);
-console.log(person.introduce());
